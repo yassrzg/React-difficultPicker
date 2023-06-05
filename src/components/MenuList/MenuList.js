f22aeea699ba7db1f88d20dc63c46264ec8f9474
yassine.rezgui <yass.srgrzg@gmail.com>
2023-06-05 16:11:12 +0200

    function map

--- src/components/MenuList/MenuList.js
+++ src/components/MenuList/MenuList.js
@@ -1,29 +1,21 @@
 import React from 'react'
 import MenuListItem from '../MenuListItem/MenuListItem'
 import style from './style.module.css'
+import { DIFFICULTIES } from './constant'
 
 export default function MenuList({difficulty, onItemClick, isSelected}) {
+
+
   return (
     <div className={style.container}>
-        <MenuListItem 
-        onClick = {onItemClick} 
-        difficulty ="Low" 
-        isSelected = {difficulty == "Low"}/>
-
-        <MenuListItem 
-        onClick = {onItemClick} 
-        difficulty="Medium" 
-        isSelected = {difficulty == "Medium"}/>
-
-        <MenuListItem 
-        onClick = {onItemClick} 
-        difficulty ="High" 
-        isSelected = {difficulty == "High"}/>
-
-        <MenuListItem 
-        onClick = {onItemClick} 
-        difficulty ="Insane" 
-        isSelected = {difficulty == "Insane"}/>
+        {
+            DIFFICULTIES.map((diff) => (
+                <MenuListItem 
+                    onClick = {onItemClick} 
+                    difficulty ={diff} 
+                    isSelected = {difficulty == diff}/>
+            ) )
+        }
     </div>
   )
 }
---
+++ src/components/MenuList/constant.js
@@ -0,0 +1,3 @@
+
+
+export const DIFFICULTIES = ["Low", "Medium", "High", "Insane"];
